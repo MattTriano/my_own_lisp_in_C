a67d60668f3289bd40fd5eac07ca9f27acd622fd
MattTriano <matt.triano@gmail.com>
2019-07-18 22:27:17 -0500

    wrapped up chapter 2

--- hello_world_bonus.c
+++ hello_world_bonus.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+
+char put_five_for(char* x, int reps) {
+  for (int i = 0; i < reps; i++) {
+    puts(x);
+  }
+  return 0;
+}
+
+char put_five_while(char* x, int reps) {
+  int i = 0;
+  while (i < reps) {
+    puts(x);
+    i = i + 1;
+  }
+  return 0;
+}
+
+int main(int argc, char** argv) {
+  put_five_for("Hello, World! For!", 5);
+  put_five_while("Hello, World! While!", 5);
+  return 0;
+}
